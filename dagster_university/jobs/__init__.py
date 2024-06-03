98fc21b15fb9ee7aa7775ca1756906a08d1c759e
matsudan <daaamatsun@gmail.com>
2024-06-03 21:57:13 +0900

    Apply partitions to assets

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,13 +1,16 @@
 from dagster import AssetSelection, define_asset_job
+from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week
+    partitions_def=monthly_partition,
+    selection=AssetSelection.all() - trips_by_week,
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
-    selection=trips_by_week
+    partitions_def=weekly_partition,
+    selection=trips_by_week,
 )
